f64fd8d9e5a2841a9f5b7547b7dd291b43e7ac2b
SumanBhandari69 <sumanbhandari6969@gmail.com>
2025-06-20 15:08:08 +0545

    todos completed

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from app.views import home, login_view, signup_view
+from app.views import home, login_view, signup_view, add_todo, signout, delete_todo, change_status
 
 urlpatterns = [
     path("", home, name='home'),
     path("login/", login_view, name='login'),
     path("signup/", signup_view),
+    path("add-todo/", add_todo),
+    path("logout/", signout),
+    path("delete-todo/<int:id>", delete_todo),
+    path("change-status/<int:id>/<str:status>", change_status),
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login as loginuser
+from django.contrib.auth import authenticate, login as loginuser, logout
+from app.forms import TODOForm
+from app.models import TODO
+from django.contrib.auth.decorators import login_required
 # from django.contrib import messages
 
-
+@login_required(login_url='login')
 def home(request):
-    return render(request,'app/home.html')
+    if request.user.is_authenticated:
+        user = request.user
+        form = TODOForm()
+        todos = TODO.objects.filter(user = user).order_by('-priority')
+        return render(request,'app/home.html', context = {'form' : form, 'todos' : todos})
 
 def login_view(request):
     if request.method == "GET":
@@ -34,6 +41,7 @@ def login_view(request):
 
 
 def signup_view(request):
+
     if request.method=='GET':
         form = UserCreationForm()
         context = {
@@ -54,3 +62,36 @@ def signup_view(request):
         else:
             # print(form.errors) 
             return render(request,'app/signup.html', context= context)
+        
+
+@login_required(login_url='login')
+def add_todo(request):
+    if request.user.is_authenticated:
+        user = request.user
+        # print(user)
+        form = TODOForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            todo = form.save(commit=False)
+            todo.user = user
+            todo.save()
+            return redirect('home')
+        else:
+            form = TODOForm()
+        return render(request,'app/home.html', context = {'form' : form})
+    
+def signout(request):
+    logout(request)
+    return redirect('login')
+
+def delete_todo(request, id):
+    TODO.objects.get(pk=id).delete()
+    return redirect('home')
+
+def change_status(request, id, status):
+    todo = TODO.objects.get(pk=id)
+    todo.status = status
+    todo.save()
+    return redirect('home')
+
+
